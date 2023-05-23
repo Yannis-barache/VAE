4ca869fad6911fd517e33846acd37d9b103d39ca
Yannis-barache <ybarache@icloud.com>
2023-05-23 15:28:17 +0200

    ajout méthode venteUtilisateur

--- src/TestJDBC.java
+++ src/TestJDBC.java
@@ -27,5 +27,20 @@ public class TestJDBC {
         }
     }
 
-    
+    public String venteUtilisateur(int idUtilisateur) throws SQLException{
+        String resultat ="";
+        try{
+            Statement s=laConnexion.createStatement();
+            ResultSet rs= s.executeQuery("select montant , idVe, nomob from PrixVente natural join VENTE natural join OBJET where idacheteur="idUtilisateur);
+            while (rs.next()){
+                Double montant = rs.getDouble("montant");
+                int idVe= rs.getInt("idVe");
+                String nomob=rs.getString("nomob");
+                resultat= "La vente "+idVe + " concerne "+ nomob + " l'enchère est à "+montant + "\n";
+            }
+        } catch (SQLException ex){
+            throw new SQLException("L'utilisateur n'a pas de vente à son actif");
+        }
+        return resultat
+    }
 }
