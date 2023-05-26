4dd4b4e44f399e2feae2dcac6dd9df10b6e199e5
Yannis-barache <ybarache@icloud.com>
2023-05-26 10:57:27 +0200

    ajout de javadoc

--- TestSQL/ConnexionMySQL.java
+++ TestSQL/ConnexionMySQL.java
@@ -9,6 +9,14 @@ public class ConnexionMySQL {
 		Class.forName("org.mariadb.jdbc.Driver");
 	}
 
+	/**
+	 * Permet de se connecter à la base de données MySQL en utilisant le driver JDBC et les paramètres de connexion
+	 * @param nomServeur Un String correspondant au nom du serveur
+	 * @param nomBase Un String correspondant au nom de la base de données
+	 * @param nomLogin Un String correspondant au nom du login
+	 * @param motDePasse Un String correspondant au mot de passe
+	 * @throws SQLException Si la connexion échoue
+	 */
 	public void connecter(String nomServeur, String nomBase, String nomLogin, String motDePasse) throws SQLException {
 		this.mysql=null;
 		this.connecte=false;
@@ -16,22 +24,48 @@ public class ConnexionMySQL {
 					"jdbc:mysql://"+nomServeur+":3306/"+nomBase,nomLogin, motDePasse);
 		this.connecte=true;
 	}
+
+
 	public void close() throws SQLException {
 		this.mysql.close();
 		this.connecte=false;
 	}
 
+
+	/**
+	 * Permet de savoir si la connexion est établie
+	 * @return Un booléen indiquant si la connexion est établie, true si c'est le cas, false sinon
+	 */
     public boolean isConnecte() { return this.connecte;}
 	
+	/**
+	 * Permet de créer un objet Blob
+	 * @return Un objet Blob 
+	 * @throws SQLException Si la création échoue
+	 * @see Blob
+	 */
     public Blob createBlob()throws SQLException{
 		return this.mysql.createBlob();
 	}
+
+	/**
+	 * Permet de créer un objet Statement
+	 * @return Un objet Statement permettant d'effectuer des requêtes SQL
+	 * @throws SQLException Si la création échoue
+	 * @see Statement
+	 */
 	public Statement createStatement() throws SQLException {
 		return this.mysql.createStatement();
 	}
 
+	/**
+	 * Permet de créer un objet PreparedStatement
+	 * @param requete Un String correspondant à la requête SQL
+	 * @return Un objet PreparedStatement permettant d'effectuer des requêtes SQL
+	 * @throws SQLException Si la création échoue
+	 * @see PreparedStatement
+	 */
 	public PreparedStatement prepareStatement(String requete) throws SQLException{
 		return this.mysql.prepareStatement(requete);
 	}
-	
 }
--- TestSQL/TestJDBC.java
+++ TestSQL/TestJDBC.java
@@ -9,20 +9,31 @@ public class TestJDBC {
     public String venteAVenir() throws SQLException{
         String resultat="";
         try{
+            // Création de la requête
             Statement s=laConnexion.createStatement();
+
+            // La requête qui nous intéresse
             ResultSet rs=s.executeQuery("select nomob, prixbase, prixmin,debutve,finve,pseudout from UTILISATEUR natural join OBJET natural join VENTE where idst=1");
+           
+            //Récupération des données
             while (rs.next()){
-                String nomob=rs.getString("nomob");
+                
+                String nomob=rs.getString("nomob"); 
                 Double prixbase= rs.getDouble("prixbase");
                 Double prixmin= rs.getDouble("prixmin");
                 String debutVente= rs.getString("debutve");
                 String finVente=rs.getString("finve");
                 String pseudoUtilisateur=rs.getString("pseudout");
-                resultat+= nomob + " " + prixbase + " " + " " + prixmin + " " +debutVente + " " + finVente + " " + pseudoUtilisateur + "\n" + "\n";
+
+                // Ajout des données dans la chaine de caractère
+                resultat+= nomob + " " + prixbase + " " + " " + prixmin +
+                " " +debutVente + " " + finVente + " " + pseudoUtilisateur + "\n" + "\n";
             }
+            rs.close();
             return resultat;
         }
         catch (SQLException ex){
+            // Si aucune vente n'est à venir donc si aucune vente n'a un idst=1
             throw new SQLException("Aucune vente à venir");
         }
     }
@@ -30,17 +41,30 @@ public class TestJDBC {
     public String venteUtilisateur(int idUtilisateur) throws SQLException{
         String resultat ="";
         try{
+            // Création de la requête
             Statement s=laConnexion.createStatement();
+            
+            // La requête qui nous intéresse
             ResultSet rs= s.executeQuery("select * from PrixVentePasFini natural join OBJET natural join VENTE  where idut=" + idUtilisateur);
+            
+            //Récupération des données
             while (rs.next()){
                 Double montant = rs.getDouble("montant");
                 int idVe= rs.getInt("idve");
                 String nomob=rs.getString("nomob");
-                resultat+= "La vente numéro "+idVe + " concerne "+ nomob + " l'enchère la plus haute est à "+montant + " euros." +"\n";
+
+                // Ajout des données dans la chaine de caractère
+                resultat+= "La vente numéro "+idVe + " concerne "+ nomob + 
+                " l'enchère la plus haute est à "+montant + " euros." +"\n";
+                
             }
-        } catch (SQLException ex){
+            rs.close();
+            return resultat;
+            
+        } catch (SQLException ex){ 
+            // Si l'utilisateur n'a pas de vente à son actif
             throw new SQLException("L'utilisateur n'a pas de vente à son actif");
         }
-        return resultat;
+        
     }
 }
