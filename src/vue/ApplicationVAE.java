5884d8a7c2598e18e47fd7e8b216358f5fa5044e
jordanlavenant <jordan.lavenant@etu.univ-orleans.fr>
2023-06-14 22:41:43 +0200

    switch

--- src/vue/ApplicationVAE.java
+++ src/vue/ApplicationVAE.java
@@ -4,6 +4,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.Border;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.Pane;
@@ -11,33 +12,34 @@ import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class ApplicationVAE extends Application{
+
+    private Scene scene;
     
     @Override
     public void init() {
 
     }
 
-    private Scene Scene() {
-        BorderPane root = new BorderPane();
+    public void fenetreAccueil() {
+        BorderPane root = new FenetreAccueil(this);
         root.setBackground(new Background(new BackgroundFill(Color.web("white"),CornerRadii.EMPTY,Insets.EMPTY)));
-        // root.setTop();
-        root.setCenter(new FenetreConnexion());
-        return new Scene(root,1800,1000);   
+        this.scene.setRoot(root);
+    }
+
+    public void fenetreRegiser() {
+        BorderPane root = new FenetreInscription(this);
+        root.setBackground(new Background(new BackgroundFill(Color.web("white"),CornerRadii.EMPTY,Insets.EMPTY)));
+        this.scene.setRoot(root);
     }
 
 
-
-
-
-
-
-
-
-    
     @Override
     public void start(Stage stage) {
+        BorderPane root = new FenetreConnexion(this);
+        root.setBackground(new Background(new BackgroundFill(Color.web("white"),CornerRadii.EMPTY,Insets.EMPTY)));
+        this.scene = new Scene(root,1800,1000);
+        stage.setScene(this.scene);
         stage.setTitle("VAE - Groupe C");
-        stage.setScene(this.Scene());
         //Vue de base
         stage.show();
     }
