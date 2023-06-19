86b52e50b3f37e04bf2dbc68bc220aeece17ed57
Ya.nnis <ybarache@icloud.com>
2023-06-19 09:53:03 +0200

    Fonctionnement de la connexion bd

--- TestSQL/ExecutableTest.java
+++ TestSQL/ExecutableTest.java
@@ -7,24 +7,19 @@ public class ExecutableTest{
             try{
                 laConnexion.connecter("servinfo-mariadb", "DBbarache", "barache", "barache");
                 TestJDBC test= new TestJDBC(laConnexion);
+                ScriptJDBC test2= new ScriptJDBC(laConnexion);
                 String res= test.venteAVenir();
                 System.out.println("Exemple des ventes à venir \n  ");
                 System.out.println(res);
                 System.out.println("-------------------------------------------- \n" + "\n");
                 res=test.venteUtilisateur(707);
                 System.out.println(res);
+                boolean connecter= test2.connexion("ddzedzed","ezdezfdze");
+                System.out.println(connecter);
+                laConnexion.close();
     
             } catch (SQLException ex){
                 System.out.println("Erreur SQL : " + ex.getMessage());
-            }
-            finally{
-                try{
-                    laConnexion.close();
-                }
-                catch(SQLException ex){
-                    System.out.println("Erreur SQL : " + ex.getMessage());
-                }
-
             }
 
         
