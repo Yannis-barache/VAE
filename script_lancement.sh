c54ab2c95bc0e953253628d8a03ea22170746e08
Ya.nnis <ybarache@icloud.com>
2023-06-19 09:05:46 +0200

    déplacement temporaire + essai script

---
+++ script_lancement.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+javac --module-path /usr/share/openjfx/lib/ --add-modules javafx.controls -d ./bin ./src/*.java
+java --module-path /usr/share/openjfx/lib/ --add-modules javafx.controls -cp bin:img ApplicationVAE
