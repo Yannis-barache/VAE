4e41d34f9cffb4e3f97e3ff9d61b9df09652b677
Ya.nnis <ybarache@icloud.com>
2023-06-20 09:18:18 +0200

    Ajout vérif email

--- src/ControleurInscription.java
+++ src/ControleurInscription.java
@@ -22,10 +22,13 @@ public class ControleurInscription implements EventHandler<ActionEvent>{
         if (!Valide.memeMDP(mdp1, mdp2)){
             this.fenetreInscription.setAlertLogin("Veuillez rentrez les mêmes mot de passe");
         }
+        if (!Valide.emailValide(mail)){
+            this.fenetreInscription.setAlertLogin("Veuillez rentrez un mail valide");
+        }
         else{
             try{
                 this.fenetreInscription.setAlertLogin("Insertion en cours");
-                this.appli.getUtilisateurBD().insererUtilisateur(new Utilisateur(2, pseudo, mail, mdp1, true, false));
+                this.appli.getUtilisateurBD().insererUtilisateur(new Utilisateur(45, pseudo, mail, mdp1, true, false));
                 this.fenetreInscription.setAlertLogin("Insertion réussie");
 
             } catch(SQLException ex){
--- src/Valide.java
+++ src/Valide.java
@@ -11,7 +11,24 @@ public class Valide {
     }
     
 
-    public static boolean valideAdresse(String adresse){
+    public static boolean emailValide(String email) {
+        int indexSymbole = email.indexOf('@');
+        int indexPoint = email.indexOf('.', indexSymbole);
+
+        if (indexSymbole < 0 || indexPoint < 0) {
+            return false; // L'adresse e-mail doit contenir à la fois le symbole @ et le symbole .
+        }
+
+        // Vérifier qu'il n'y a pas de symboles supplémentaires avant ou après le @
+        if (indexSymbole != email.lastIndexOf('@') || indexSymbole == 0 || indexSymbole == email.length() - 1) {
+            return false;
+        }
+
+        // Vérifier qu'il n'y a pas de symboles supplémentaires avant ou après le .
+        if (indexPoint != email.lastIndexOf('.') || indexPoint == indexSymbole + 1 || indexPoint == email.length() - 1) {
+            return false;
+        }
+
         return true;
     }
 
